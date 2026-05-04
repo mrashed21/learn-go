6c71a38a42d83600dedf4a2996fb4c671bc26d28
mrashed21 <rashedjaman768@gmail.com>
2026-05-04 17:33:39 +0600

    add comments to clarify variable scope in variable function

--- variavle.go
+++ variavle.go
@@ -4,11 +4,13 @@ package main
 import "fmt"
 
 func variable() {
-	age := 30
+	age := 30 // outer variable
 	name := func() {
-		user_name := "Rashed"
+		user_name := "Rashed" // inner variable
 		fmt.Printf("User Name: %s\n Age: %d", user_name, age)
 	}
 	name()
+
+	/* outer inner e access korte parbe but inner theke outer access kora jabe na.*/
 
 }
