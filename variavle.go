5e84469810ec9bc3e48411b0d545f5f9a3b41755
mrashed21 <rashedjaman768@gmail.com>
2026-05-04 17:46:19 +0600

    fix: add newline to output formatting in variable function

--- variavle.go
+++ variavle.go
@@ -7,10 +7,18 @@ func variable() {
 	age := 30 // outer variable
 	name := func() {
 		user_name := "Rashed" // inner variable
-		fmt.Printf("User Name: %s\n Age: %d", user_name, age)
+		fmt.Printf("User Name: %s\n Age: %d\n", user_name, age)
 	}
 	name()
 
 	/* outer inner e access korte parbe but inner theke outer access kora jabe na.*/
 
+	// variable shadowing
+	Age := 25
+	shadowing := func() {
+		Age = 20                                // this age variable shadows the outer age variable
+		fmt.Printf("\nShadowed Age: %d\n", Age) // prints 20
+	}
+	shadowing()
+	fmt.Printf("Outer Age: %d\n", Age) // prints 20
 }
